77e2a2ec4ef946a8a67184e4510b3b64dc8497c7
Figueroa Rodríguez Luis Gerardo <201510450@tese.edu.mx>
2022-06-21 23:59:33 -0500

    seagregaronbanderas

--- src/app/iniciohome/navbar/navbar.component.ts
+++ src/app/iniciohome/navbar/navbar.component.ts
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  banderabuscar: boolean= false;
   constructor() { }
-
+  buscar(){
+    if (this.banderabuscar == false){
+      this.banderabuscar= true;
+    }else{
+      this.banderabuscar = false;
+    }
+  }
   ngOnInit(): void {
   }
 
